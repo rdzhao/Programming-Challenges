196e4c63e7a58b4998540e37b756cf5eafe2eb95
zhaorund <zhaorund@msu.edu>
2018-03-10 15:53:13 -0500

    init

---
+++ 1.6.5/Problem.cpp
@@ -0,0 +1,29 @@
+#include "Problem.h"
+
+void Problem::launch()
+{
+	read();
+	cout << "Reading Complete!" << endl;
+	solve();
+	cout << "Solving Complete!" << endl;
+	output();
+	cout << "Output Complete!" << endl;
+}
+
+void Problem::read()
+{
+	// TODO: implement details to read input
+
+}
+
+void Problem::solve()
+{
+	// TODO: implement details of the algorithm
+
+}
+
+void Problem::output()
+{
+	// TODO: implement details of output
+
+}
---
+++ 1.6.5/Problem.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+class Problem
+{
+public:
+	Problem(std::string fn) { filename = fn; };
+	~Problem() {};
+
+	void launch(); 
+
+	void read();
+	void solve();
+	void output();
+
+	// TODO: additional helper functions
+
+
+private:
+	std::string filename;
+	
+	// TODO: set input parameters here as private member variables.
+
+};
